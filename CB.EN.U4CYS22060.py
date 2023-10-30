fa1e8cdaecc5e63bbc811995483a6217219ffd31
Shree Harini <hharini441@gmail.com>
2023-10-30 16:14:38 +0530

    Add files via upload

---
+++ CB.EN.U4CYS22060.py
@@ -0,0 +1,15 @@
+my=["Harini"]
+my.insert(0,"Chakri")
+print(my)
+my.append("Sharvesh")
+print(my)
+my.append("Shravan")
+print(my)
+my.insert(0,"Vamsi")
+print(my)
+my.pop(4)
+print(my)
+my.remove("Vamsi")
+print(my)
+my.clear()
+del my
